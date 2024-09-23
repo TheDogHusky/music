076178e1af1c2262350d00f70a878671abad92ea
TheDogHusky <contact@classydev.fr>
2024-09-23 15:06:29 +0200

    feat: Moved button variants to functions.ts

--- utils/functions.ts
+++ utils/functions.ts
@@ -7,7 +7,7 @@ export function useButtonVariants(props: ButtonProps | ButtonLinkProps | NuxtBut
         secondary: "hover:bg-secondary border-secondary focus:ring-secondary text-secondary hover:text-neutral-950",
         warning: "hover:bg-amber-600 border-amber-600 focus:ring-amber-600 text-amber-600 hover:text-white",
         danger: "hover:bg-red-700 border-red-700 focus:ring-red-700 text-red-700 hover:text-white",
-        icon: "rounded-none border-s-0 border-t-0 border-e-0 text-gray-300 border-b-2 pb-2 border-dashed border-secondary hover:text-gray-100 hover:border-gray-300 hover:scale-105 h-10 w-10 hover:bg-transparent text-2xl p-2 flex flex-row items-center justify-center",
+        icon: "rounded-none border-s-0 border-t-0 border-e-0 text-gray-300 border-b-2 pb-2 border-dashed border-secondary hover:text-gray-100 hover:border-gray-300 hover:scale-105 h-auto w-auto hover:bg-transparent text-2xl p-2 flex flex-row items-center justify-center",
     };
 
     const sizeVariants: {[key: string]: string} = {
