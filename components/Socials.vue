1546758e2d03895b06b3c3ede82cc36a8a204757
TheDogHusky <contact@classydev.fr>
2024-09-23 15:06:00 +0200

    feat: Finished socials element

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,20 +1,42 @@
 <script setup lang="ts">
 import ButtonLink from "~/components/ButtonLink.vue";
+import { twMerge } from "tailwind-merge";
 
-const props = defineProps(["type"]);
+const props = defineProps<{
+  type?: string;
+}>();
 
-const types = ["footer", "header", "contact"]
+const types = ["footer", "header", "contact"];
 const type = types.includes(props.type) ? props.type : "header";
+
+const socialsSizes: {[key: string]: string} = {
+  footer: "!text-2xl",
+  header: "!text-6xl gap-4",
+  contact: "!text-8xl"
+};
+
+const buttonSocialsSizes: {[key: string]: string} = {
+  footer: "!text-2xl",
+  header: "!text-5xl",
+  contact: "!text-8xl"
+};
+
+const socialsClass = computed(() => {
+  return twMerge(
+      "flex flex-row gap-2 justify-center items-center text-gray-300 !text-4xl",
+      socialsSizes[type]
+  );
+});
 </script>
 
 <template>
-  <div class="flex flex-row gap-2 justify-center items-center text-gray-300 text-4xl">
-    <ButtonLink href="https://www.twitter.com/ClassyCraft3r" target="_blank" variant="icon"><Icon name="nf-cod-twitter" /></ButtonLink>
-    <ButtonLink href="https://www.instagram.com/adambillardmusic/" target="_blank" variant="icon"><Icon name="nf-fa-instagram" /></ButtonLink>
-    <ButtonLink href="https://www.youtube.com/@adambillardofficial" target="_blank" variant="icon"><Icon name="nf-fa-youtube" /></ButtonLink>
-    <ButtonLink href="https://discord.gg/d7dFSBcumt" target="_blank" variant="icon"><Icon name="nf-fa-discord" /></ButtonLink>
-    <ButtonLink href="mailto://contact@classydev.fr" target="_blank" variant="icon"><Icon name="nf-oct-mail" /></ButtonLink>
-    <ButtonLink href="https://classydev.fr" target="_blank" variant="icon"><Icon name="nf-oct-link" /></ButtonLink>
+  <div :class="socialsClass">
+    <ButtonLink :class="buttonSocialsSizes[type]" href="https://www.twitter.com/ClassyCraft3r" target="_blank" variant="icon"><Icon name="nf-cod-twitter" /></ButtonLink>
+    <ButtonLink :class="buttonSocialsSizes[type]" href="https://www.instagram.com/adambillardmusic/" target="_blank" variant="icon"><Icon name="nf-fa-instagram" /></ButtonLink>
+    <ButtonLink :class="buttonSocialsSizes[type]" href="https://www.youtube.com/@adambillardofficial" target="_blank" variant="icon"><Icon name="nf-fa-youtube" /></ButtonLink>
+    <ButtonLink :class="buttonSocialsSizes[type]" href="https://discord.gg/d7dFSBcumt" target="_blank" variant="icon"><Icon name="nf-fa-discord" /></ButtonLink>
+    <ButtonLink :class="buttonSocialsSizes[type]" href="mailto://contact@classydev.fr" target="_blank" variant="icon"><Icon name="nf-oct-mail" /></ButtonLink>
+    <ButtonLink :class="buttonSocialsSizes[type]" href="https://classydev.fr" target="_blank" variant="icon"><Icon name="nf-oct-link" /></ButtonLink>
   </div>
 </template>
 
