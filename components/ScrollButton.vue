247ab40c5b4456149428cb070f30871e9e535087
TheDogHusky <contact@classydev.fr>
2024-09-30 13:46:40 +0200

    feat: make the scroll button and base content

---
+++ components/ScrollButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  href?: string;
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <a v-bind="props" class="animate-bounce text-4xl rounded-full bg-transparent hover:bg-secondary hover:text-neutral-950 duration-200 focus:ring focus:ring-secondary focus:ring-opacity-50 m-0 p-0 flex justify-center items-center border-2 border-secondary text-secondary bottom-4 absolute">
+    <Icon name="nf-oct-arrow_down" />
+  </a>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Socials from "~/components/Socials.vue";
+import ScrollButton from "~/components/ScrollButton.vue";
 </script>
 
 <template>
@@ -7,7 +8,7 @@ import Socials from "~/components/Socials.vue";
     <NuxtImg src="/logo.webp" alt="logo" class="w-40 h-40" />
     <h1 class="cursor-default border-b-4 border-transparent hover:border-secondary border-dashed pb-2 text-5xl font-normal text-white hover:text-primary duration-200 group"><span class="duration-200 group-hover:text-secondary font-semibold text-primary">Adam Billard</span>, young musician.</h1>
     <Socials type="header" />
-    <a href="#content" class="animate-bounce text-4xl rounded-full bg-transparent hover:bg-secondary hover:text-neutral-950 duration-200 focus:ring focus:ring-secondary focus:ring-opacity-50 m-0 p-0 flex justify-center items-center border-2 border-secondary text-secondary bottom-4 absolute"><Icon name="nf-oct-arrow_down" /></a>
+    <ScrollButton href="#content" />
   </main>
   <section id="content" class="flex flex-col items-center justify-center w-full text-white h-screen bg-neutral-900 gap-2">
     <h2 class="text-2xl font-bold">My New Song</h2>
