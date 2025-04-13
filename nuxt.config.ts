4a6175763ec6a1d8b2ecf35190452639d65211e3
TheDogHusky <contact@classydev.fr>
2025-04-13 21:53:37 +0200

    feat: move to tailwind 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
-  typescript: {
-    strict: true
-  },
-  router: {
-    linkActiveClass: 'text-secondary',
-    linkExactActiveClass: 'text-red'
-  },
-  plugins: [{ src: '~/plugins/aos.client', mode: 'client' }]
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: false },
+    modules: ['@nuxt/image'],
+    css: ['~/assets/css/tailwind.css'],
+    typescript: {
+        strict: true
+    },
+    router: {
+        linkActiveClass: 'text-secondary',
+        linkExactActiveClass: 'text-red'
+    },
+    plugins: [{ src: '~/plugins/aos.client', mode: 'client' }],
+    vite: {
+        plugins: [
+            tailwindcss()
+        ]
+    }
 });
--- utils/functions.ts
+++ utils/functions.ts
@@ -1,8 +1,8 @@
-import type { ButtonProps, ButtonLinkProps, NuxtButtonLinkProps } from "../components/button.types";
+import type { ButtonProps, ButtonLinkProps, NuxtButtonLinkProps } from "~/components/button.types";
 import { twMerge } from "tailwind-merge";
 
 export function useButtonVariants(props: ButtonProps | ButtonLinkProps | NuxtButtonLinkProps): string {
-    const themeVariants: {[key: string]: string} = {
+    const themeVariants: { [key: string]: string } = {
         primary: "hover:bg-primary border-primary focus:ring-primary text-primary hover:text-neutral-950",
         secondary: "hover:bg-secondary border-secondary focus:ring-secondary text-secondary hover:text-neutral-950",
         warning: "hover:bg-amber-600 border-amber-600 focus:ring-amber-600 text-amber-600 hover:text-white",
@@ -16,10 +16,15 @@ export function useButtonVariants(props: ButtonProps | ButtonLinkProps | NuxtBut
         lg: "px-6 py-3 text-lg",
     };
 
+    const specialVariants: { [key: string]: string } = {
+        socials: "font-normal"
+    }
+
     return twMerge(
-        "rounded-none duration-200 border-2 border-gray-300 outline-none focus:ring text-base text-gray-300 font-semibold focus:ring-gray-300 focus:ring-opacity-50 bg-transparent hover:bg-gray-300 hover:text-neutral-950",
+        "rounded-none duration-200 border-2 border-gray-300 outline-none focus:ring-3 text-base text-gray-300 font-semibold focus:ring-gray-300 focus:ring-opacity-50 bg-transparent hover:bg-gray-300 hover:text-neutral-950 flex flex-row items-center justify-center gap-4",
         sizeVariants[props.size || "md"],
         themeVariants[props.variant || ""],
+        specialVariants[props.special || ""],
         props.disabled ? "opacity-50 cursor-not-allowed" : ""
     );
 }
