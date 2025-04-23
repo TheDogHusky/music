5c42d3022f870f9b55b7b99c7c1172dde76100f7
TheDogHusky <contact@classydev.fr>
2025-04-23 19:16:34 +0200

    fix: animations

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="flex flex-col items-center justify-center w-full text-white h-main bg-slate-950 gap-2 app-header">
         <h1 data-aos-easing="ease-in-out" data-aos="fade-down" data-aos-duration="500" class="group">Contact <span class="special">Me</span></h1>
-        <Socials data-aos-easing="ease-in-out" data-aos="fade-down" data-aos-duration="500" data-aos-delay="100" type="contact" />
+        <Socials data-aos-easing="ease-in-out" data-aos="fade-down" data-aos-duration="500" data-aos-delay="200" type="contact" />
     </main>
 </template>
