b00ec67627b5b6226ee67a83c58acade80dc6f54
TheDogHusky <contact@classydev.fr>
2025-09-04 20:30:06 +0200

    refactor: change main page layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,9 +4,7 @@
 
 <template>
     <main class="flex flex-col items-center justify-center w-full text-white bg-slate-950 gap-2 app-header">
-        <div data-aos-easing="ease-in-out" data-aos-delay="100" data-aos="fade-right" data-aos-duration="1000" class="flex flex-row gap-6 items-start justify-center w-auto">
-            <NuxtImg placeholder src="/songs/ttc.png" alt="Time To Cry cover" class="w-64 h-64 rounded-md shadow-lg" />
-            <h1 class="group"><a target="_blank" href="https://distrokid.com/hyperfollow/adambillard/time-to-cry" class="special">Time To Cry</a><span>is here.</span></h1>
-        </div>
+        <h1 class="group"><a target="_blank" href="https://distrokid.com/hyperfollow/adambillard/time-to-cry" class="special">Time To Cry</a> is here.</h1>
+        <NuxtImg placeholder src="/songs/ttc.png" alt="Time To Cry cover" class="w-1/4 h-1/4 rounded-md shadow-lg" />
     </main>
 </template>
